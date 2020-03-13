788a195dd6b9932e0aa5eeac56d0955163179b43
Jeremia Christian <jejechristianap@gmail.com>
2020-03-14 01:21:50 +0700

    Splash Screen DUMI

---
+++ app/src/main/java/com/fidac/dumi/Splash.java
@@ -0,0 +1,26 @@
+package com.fidac.dumi;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+
+public class Splash extends Activity {
+    private final int SPLASH_DISPLAY_LENGTH = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_dumi);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(Splash.this, MainActivity.class);
+                Splash.this.startActivity(mainIntent);
+                Splash.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
