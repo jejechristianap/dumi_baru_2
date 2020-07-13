12ca176643a59a8b06cf5434b152a2021b9f550c
Jeremia Christian <jejechristianap@gmail.com>
2020-07-13 11:53:32 +0700

    Bottom sheet fragment testing

---
+++ app/src/main/java/com/minjem/dumi/util/BottomSheetFragment.kt
@@ -0,0 +1,32 @@
+package com.minjem.dumi.util
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.minjem.dumi.R
+import kotlinx.android.synthetic.main.bottom_sheet_layout.*
+
+class BottomSheetFragment: BottomSheetDialogFragment(){
+    private var fragmentView: View? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        fragmentView = inflater.inflate(R.layout.bottom_sheet_layout, container, false)
+        return fragmentView
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+    }
+
+    private fun initView() {
+        bTutup.setOnClickListener {
+
+        }
+    }
+}
